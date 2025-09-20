9fd4a84031efe15efc348b5a8e379e65900e0c0b
Fiskoff <fiskoff2015@yandex.ru>
2025-09-20 11:35:12 +0700

    Зарегистрированы модели и настройка отображения моделей в админке

--- culinary_portal/admin.py
+++ culinary_portal/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Передавать поля можно списком или кортежем. Рекомендовано - картеж, так как он занимает меньше памяти.
+    list_display = ("id", "title", "watched", "is_published", "category") # Поля для быстрого просмотра
+    list_display_links = ("title",) # Кликабельное поля для перехода к редактированию
+    list_editable = ("is_published",) # Поле которое можно изменять при быстром просмотре не заходя в редактирование записи
+    readonly_fields = ("watched",) # Поля которые админ не может изменять
+    list_filter = ("category", "is_published")
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
