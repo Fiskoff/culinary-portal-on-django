558cb52acb0650767b85c5bcd8a5134fe5928e8e
Fiskoff <fiskoff2015@yandex.ru>
2025-09-21 10:04:40 +0700

    Created index view

--- culinary_portal/views.py
+++ culinary_portal/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category, Post
+
+
+#  Представление для главной страницы
+def get_index(request):
+    """Для главной страницы"""
+    posts = Post.objects.all()  # SELECT * FROM post
+    categories = Category.objects.all()
+    context = {
+        "title": "Главая страница",
+        "posts": posts,
+        "categories": categories,
+    }
+    # Возвращаем запрос пользователя, шаблон и данные для формирования страницы
+    return render(request, "cooking/index.html", context)
